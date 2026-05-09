982a8d3e973438960ec0b76111ecbe4513426ef6
stevzhang <stevzhang@gmail.com>
2026-05-09 17:25:49 +0800

    feat: add palindrome package with rune-safe IsPalindrome
    Closes #13

---
+++ palindrome/palindrome.go
@@ -0,0 +1,11 @@
+package palindrome
+
+func IsPalindrome(s string) bool {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		if runes[i] != runes[j] {
+			return false
+		}
+	}
+	return true
+}
